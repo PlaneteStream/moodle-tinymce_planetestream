96820973ce792d6546d3e83a66b00b9afa701edb
Planet eStream <support@planetestream.co.uk>
2023-02-16 09:48:58 +0000

    added missing line from last fix

---
+++ planetestream/lang/en/tinymce_planetestream.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ * Planet eStream tinymce Plugin
+ *
+ * @since 2.0
+ * @package    tinymce_planetestream
+ * @copyright  Planet Enterprises Ltd
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ *
+ */
+/**
+ * Strings for component 'tinymce_planetestream', language 'en', branch 'MOODLE_20_STABLE'
+ */
+$string['pluginname'] = 'Planet eStream';
+$string['privacy:metadata:tinymce_planetestream'] = 'In order to integrate with Planet eStream, user information must be relayed.';
+$string['privacy:metadata:tinymce_planetestream:userid'] = 'Your user ID is used to find your email address.';
+$string['privacy:metadata:tinymce_planetestream:userip'] = 'Your IP address is used as a security measure when sending data between Moodle and Planet eStream.';
+$string['privacy:metadata:tinymce_planetestream:email'] = 'Your email is used to complete authentication between Moodle and Planet eStream.';
