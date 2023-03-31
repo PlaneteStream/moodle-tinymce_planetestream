77cef975e30be565b9714c700b97d268b8f2184d
Planet eStream <support@planetestream.co.uk>
2023-03-31 13:18:55 +0100

    Add files via upload

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 $plugin            = new StdClass();
 $plugin->component = 'tinymce_planetestream';
-$plugin->version   = 202303320;
+$plugin->version   = 2023040100;
 $plugin->requires  = 2013111800; // Moodle 2.6
 $plugin->release   = '6.542';
 $plugin->maturity  = MATURITY_STABLE;
