00e3cf76e968c06a345ce37c0ee20dd36e82c0ae
Planet eStream <support@planetestream.co.uk>
2024-04-08 14:50:48 +0100

    Removed unnecessary hyperlinks for WCAG

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 defined('MOODLE_INTERNAL') || die();
 $plugin            = new StdClass();
 $plugin->component = 'tinymce_planetestream';
-$plugin->version   = 2024011000;
+$plugin->version   = 2024040800;
 $plugin->requires  = 2013111800; // Moodle 2.6
-$plugin->release   = '6.55';
+$plugin->release   = '6.56';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->dependencies = array(
     'assignsubmission_estream' => ANY_VERSION,
